acfcc1af9880b40e35fd0aa4c0435edc974689f2
Choster900 <16adonaysergio@gmail.com>
2024-10-23 15:37:40 -0600

    pintando informacion de productos

--- assets/js/DetalleProducto.js
+++ assets/js/DetalleProducto.js
@@ -1,12 +1,132 @@
 $(document).ready(function () {
-    
+
+    function getParameterByName(name) {
+        const url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+        const results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+    // Obtener el 'id' del producto desde la URL
+    const productId = getParameterByName('id');
+
+    console.log(productId);
+
     $.ajax({
         type: "GET",
-        url: baseURL + "/products?product_id=1",
+        url: baseURL + "/products/" + productId,
         dataType: "json",
-        success: function (response) {
-            console.log(response);
-            
+        success: function (productResponse) {
+            console.log(productResponse);
+    
+            // Calcular el porcentaje de descuento si el precio de descuento existe
+            let discountPercentage = 0;
+            if (productResponse.discount_price && productResponse.discount_price < productResponse.product_price) {
+                discountPercentage = ((productResponse.product_price - productResponse.discount_price) / productResponse.product_price) * 100;
+                discountPercentage = discountPercentage.toFixed(0); // Redondear a número entero
+            }
+    
+            // Actualizar el nombre del producto
+            $("#product_name").text(productResponse.product_name);
+    
+            // Actualizar el precio con descuento si existe, si no, mostrar el precio original
+            $("#product_price").html(`<sub>$</sub>${productResponse.discount_price || productResponse.product_price}`);
+    
+            // Si no hay descuento, oculta el precio original y el porcentaje
+            if (!productResponse.discount_price) {
+                $("#discount_price").hide(); // Si no hay descuento, ocultar el precio original
+            } else {
+                $("#discount_price").show(); // Mostrar el precio original si existe descuento
+                $("#discount_price").text(productResponse.product_price); // Mostrar el precio original
+            }
+    
+            // Mostrar el porcentaje de descuento si existe
+            if (discountPercentage > 0) {
+                $(".product-discount").text(`${discountPercentage}% Off`);
+            } else {
+                $(".product-discount").text(""); // Si no hay descuento, ocultar
+            }
+    
+            // Actualizar la descripción del producto
+            $("#product_description").text(productResponse.product_description);
+    
+            // Actualizar la disponibilidad del stock
+            if (productResponse.available_stock <= 0) {
+                $("#available_stock").text("Out of stock");
+                $("#available_stock").css("color", "red");
+            } else {
+                $("#available_stock").text("In stock");
+                
+            }
+            $("#amount-stock").text(productResponse.available_stock + " pieces available");
+    
+            // Obtener comentarios
+            $.ajax({
+                type: "GET",
+                url: baseURL + "/comments?product_id=" + productId,
+                dataType: "json",
+                success: function (commentsResponse) {
+                    const promedioRating = calcularPromedioRating(commentsResponse);
+                    $("#stars-rating-detail-product").html(generateStars(promedioRating));
+                },
+                error: function (error) {
+                    console.error("Error al obtener los comentarios:", error);
+                }
+            });
+    
+            // Obtener la marca
+            $.ajax({
+                type: "GET",
+                url: baseURL + "/brands/" + productResponse.brand_id,
+                dataType: "json",
+                success: function (response) {
+                    $("#brand").html(response.brand_name);
+                }
+            });
+
+
+            $.ajax({
+                type: "GET",
+                url: baseURL + "/categories/" + productResponse.category_id,
+                dataType: "json",
+                success: function (response) {
+                    $("#category").html(response.response.category_name);
+                }
+            });
+        },
+        error: function (error) {
+            console.error("Error al obtener el producto:", error);
         }
     });
+    
+
+
+    function generateStars(rating) {
+        let starsHTML = '';
+        for (let i = 1; i <= 5; i++) {
+            if (i <= Math.floor(rating)) {
+                starsHTML += '<span class="star-icon las la-star active"></span>';
+            } else if (i === Math.ceil(rating) && rating % 1 !== 0) {
+                starsHTML += '<span class="star-icon las la-star-half-alt active"></span>';
+            } else {
+                starsHTML += '<span class="star-icon las la-star"></span>';
+            }
+        }
+        starsHTML += `<span class="stars-rating__point">${rating}</span>
+                        <span class="stars-rating__review">
+                        <span>778</span> Reviews</span>
+                        `
+        return starsHTML;
+    }
+
+    const calcularPromedioRating = (comments) => {
+        if (comments.length === 0) return 0;
+
+        const totalRating = comments.reduce((acc, comment) => acc + comment.rating, 0);
+        return (totalRating / comments.length).toFixed(1);
+    };
+
 });
--- assets/js/Productos.js
+++ assets/js/Productos.js
@@ -1,85 +1,102 @@
 $(document).ready(function () {
+    const productList = $('#product-page-list');
 
+    // Obtener productos
     $.ajax({
         type: "GET",
         url: baseURL + "/products",
         dataType: "json",
-        success: function (response) {
+        success: function (products) {
+            products.forEach(product => {
+                const { id, product_name, product_price } = product;
 
-            const productList = $('#product-page-list');
+                // Hacer segunda petición para obtener los comentarios
+                $.ajax({
+                    type: "GET",
+                    url: baseURL + "/comments?product_id=" + id,
+                    dataType: "json",
+                    success: function (comments) {
+                        const promedioRating = calcularPromedioRating(comments);
+                        
+                        // Solo aquí se agrega el producto al DOM, ya con el rating calculado
+                        productList.append(crearProductoHTML({ id, product_name, product_price, promedioRating }));
+                    },
+                    error: function (error) {
+                        console.error("Error al obtener comentarios:", error);
+                        // En caso de error, puedes agregar el producto sin rating
+                        productList.append(crearProductoHTML({ id, product_name, product_price, promedioRating: null }));
+                    }
+                });
+            });
+        },
+        error: function (error) {
+            console.error("Error al obtener productos:", error);
+        }
+    });
 
-            response.forEach(producto => {
-                const promedioRating = calcularPromedioRating(producto.comments);
-
-                const productoHTML = `
-                    <div class="cus-xl-3 col-lg-6 col-md-11 col-12 mb-30 px-10"  >
-                        <div class="card product product--grid">
-                            <div class="h-100">
-                                <div class="product-item">
-                                    <div class="product-item__image">
-                                        <span class="like-icon">
-                                            <button type="button" class="content-center">
-                                                <i class="lar la-heart icon"></i>
-                                            </button>
-                                        </span>
-                                        <a href="product-details.html?id=${producto.product_id}"><img class="card-img-top img-fluid" src="img/digital-chair.png" alt="${producto.product_name}"></a>
+    function crearProductoHTML({ id, product_name, product_price, promedioRating }) {
+        return `
+            <div class="cus-xl-3 col-lg-6 col-md-11 col-12 mb-30 px-10">
+                <div class="card product product--grid">
+                    <div class="h-100">
+                        <div class="product-item">
+                            <div class="product-item__image">
+                                <span class="like-icon">
+                                    <button type="button" class="content-center">
+                                        <i class="lar la-heart icon"></i>
+                                    </button>
+                                </span>
+                                <a href="product-details.html?id=${id}">
+                                    <img class="card-img-top img-fluid" src="img/digital-chair.png" alt="${product_name}">
+                                </a>
+                            </div>
+                            <div class="card-body px-20 pb-25 pt-25">
+                                <div class="product-item__body text-capitalize">
+                                    <a href="product-details.html?id=${id}">
+                                        <h6 class="card-title">${product_name}</h6>
+                                    </a>
+                                    <div class="stars-rating d-flex align-items-center flex-wrap mb-10">
+                                        ${generateStars(promedioRating)}
+                                        <span class="stars-rating__point">${promedioRating || 'N/A'}</span>
+                                        <span class="stars-rating__review"><span>185</span> Reviews</span>
                                     </div>
-                                    <div class="card-body px-20 pb-25 pt-25">
-                                        <div class="product-item__body text-capitalize">
-                                            <a href="product-details.html?id=${producto.product_id}">
-                                                <h6 class="card-title">${producto.product_name}</h6>
-                                            </a>
-                                            <div class="stars-rating d-flex align-items-center flex-wrap mb-10">
-                                                ${generateStars(promedioRating)}
-                                                <span class="stars-rating__point">${promedioRating}</span>
-                                                <span class="stars-rating__review"><span>185</span> Reviews</span>
-                                            </div>
-                                        </div>
-                                        <div class="product-item__footer">
-                                            <div class="d-flex align-items-center flex-wrap">
-                                                <span class="product-desc-price">$${producto.product_price.toFixed(2)}</span>
-                                                <span class="product-price">$${producto.product_price.toFixed(2)}</span>
-                                                <span class="product-discount">${producto.product_price}</span>
-                                            </div>
-                                        </div>
-                                        <div class="product-item__button d-flex mt-20 flex-wrap">
-                                            <button class="btn btn-default btn-squared color-light btn-outline-light px-20">
-                                                <img src="img/svg/shopping-bag.svg" alt="shopping-bag" class="svg"> Add To Cart
-                                            </button>
-                                            <button class="btn btn-primary btn-default btn-squared border-0 px-25">buy now</button>
-                                        </div>
+                                </div>
+                                <div class="product-item__footer">
+                                    <div class="d-flex align-items-center flex-wrap">
+                                        <span class="product-desc-price">$${product_price.toFixed(2)}</span>
+                                        <span class="product-price">$${product_price.toFixed(2)}</span>
+                                        <span class="product-discount">${product_price}</span>
                                     </div>
+                                </div>
+                                <div class="product-item__button d-flex mt-20 flex-wrap">
+                                    <button class="btn btn-default btn-squared color-light btn-outline-light px-20">
+                                        <img src="img/svg/shopping-bag.svg" alt="shopping-bag" class="svg"> Add To Cart
+                                    </button>
+                                    <button class="btn btn-primary btn-default btn-squared border-0 px-25">Buy Now</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                `;
-        
-                productList.append(productoHTML);
-            });
-        
-
-        }
-    });
-
-    function generateStars(rating) {
-        let starsHTML = '';
-        for (let i = 1; i <= 5; i++) {
-            if (i <= Math.floor(rating)) {
-                starsHTML += '<span class="star-icon las la-star active"></span>';
-            } else if (i === Math.ceil(rating) && rating % 1 !== 0) {
-                starsHTML += '<span class="star-icon las la-star-half-alt active"></span>';
-            } else {
-                starsHTML += '<span class="star-icon las la-star"></span>';
-            }
-        }
-        return starsHTML;
+                </div>
+            </div>
+        `;
     }
 
-    const calcularPromedioRating = (comments) => {
-        if (comments.length === 0) return 0; // Si no hay comentarios, el promedio es 0
+    function generateStars(rating) {
+        return Array.from({ length: 5 }, (_, i) => {
+            if (i < Math.floor(rating)) {
+                return '<span class="star-icon las la-star active"></span>';
+            } else if (i === Math.floor(rating) && rating % 1 !== 0) {
+                return '<span class="star-icon las la-star-half-alt active"></span>';
+            } else {
+                return '<span class="star-icon las la-star"></span>';
+            }
+        }).join('');
+    }
 
-        const totalRating = comments.reduce((acc, comment) => acc + comment.rating, 0);
-        return (totalRating / comments.length).toFixed(1); // Retorna el promedio con 1 decimal
-    };
+    function calcularPromedioRating(comments) {
+        if (!comments.length) return 0;
+        const totalRating = comments.reduce((acc, { rating }) => acc + rating, 0);
+        return (totalRating / comments.length).toFixed(1);
+    }
 });
